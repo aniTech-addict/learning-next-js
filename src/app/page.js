2a15a5d816961e660419eb575734d39df7750ddc
aniTech-addict <kirti7666anand15@gmail.com>
2025-10-27 11:44:30 +0530

    Manging client side local data using response.data

--- src/app/page.js
+++ src/app/page.js
@@ -1,63 +1,7 @@
-"use client"
-import {useState} from 'react';
 
 const Page = () => {
-    const [tasks, setTasks] = useState([]);
-    const [input, setInput] = useState("");
-    function handleClick(){
-        const newTask = { id: Date.now(), text: input };
-        setTasks([...tasks, newTask]);
-        setInput("");
-    }
-
-    function deleteTask(id){
-        setTasks(tasks.filter(task => task.id !== id));
-    }
-
-    function clearAll(){
-        setTasks([]);
-    }
     return (
-        <>
-            <div>
-
-                <p className="text-6xl">Tasks</p>
-
-                <ul >
-                    {tasks.map((task)=>(
-                        <>
-                        <li key={task.id} className="flex gap-2">
-                            <p>{task.text}</p>
-                            <button className="hover:scale-105" onClick={()=>deleteTask(task.id)}>Delete</button>
-                        </li>
-                        </>
-                    ))}
-                </ul>
-
-            </div>
-
-            <div className="inline-flex gap-3">
-                <h2>
-                    Enter New Task
-                </h2>
-                <div>
-                    <input
-                    placeholder="Enter task"
-                    value = {input}
-                    onChange={(e) => setInput(e.target.value)
-                    }
-                    className='border-2 border-amber-200 p-0.5'
-                    />
-
-                    <button className='m-1' onClick={handleClick} >Add + </button>
-                </div>
-            </div>
-            <hr />
-        <div className='text-3xl'>
-            <p>Caution !</p>
-        </div>
-            <button className='hover:scale-105 hover:text-red-500' onClick={ ()=>clearAll()}>Clear All</button>
-        </>
+        <div>Home Page</div>
     )
 }
 export default Page
